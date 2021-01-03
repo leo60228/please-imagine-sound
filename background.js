95a4a39b09e228b54a9c4072c94302dcb68df76e
leo60228 <leo@60228.dev>
2021-01-03 17:23:28 -0500

    streamData

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
-const sounds = {
+const simpleSounds = {
   'https://www.blaseball.com/api/chooseIdol': 'sounds/ahhhh.mp3',
   'https://www.blaseball.com/api/eatADangPeanut': 'sounds/peanut.wav'
 };
 
-function listener(details) {
+function simpleSoundsListener(details) {
   if (new URL(details.originUrl).host !== 'www.blaseball.com') return;
 
-  if (sounds.hasOwnProperty(details.url)) {
-    const audio = new Audio(sounds[details.url]);
+  if (simpleSounds.hasOwnProperty(details.url)) {
+    const audio = new Audio(simpleSounds[details.url]);
     audio.play();
   } else {
     console.log(`unknown: ${details.url}`);
@@ -16,7 +16,42 @@ function listener(details) {
   return {};
 }
 
-browser.webRequest.onBeforeRequest.addListener(listener, {
-  urls: Object.keys(sounds),
+function streamDataListener(details) {
+  if (new URL(details.originUrl).host !== 'www.blaseball.com') return;
+
+  const filter = browser.webRequest.filterResponseData(details.requestId);
+  const decoder = new TextDecoder("utf-8");
+
+  let buf = '';
+
+  filter.ondata = event => {
+    filter.write(event.data);
+
+    const str = decoder.decode(event.data, {stream: true});
+    buf += str;
+    const [line, rest] = buf.split('\n');
+
+    if (typeof rest === 'undefined') return;
+
+    buf = rest;
+
+    if (!line.startsWith('data: ')) return;
+
+    const json = line.replace(/^data: /, '');
+
+    const data = JSON.parse(json);
+    console.log(data);
+  };
+
+  return {};
+}
+
+browser.webRequest.onBeforeRequest.addListener(simpleSoundsListener, {
+  urls: Object.keys(simpleSounds),
   types: ['xmlhttprequest']
 });
+
+browser.webRequest.onBeforeRequest.addListener(streamDataListener, {
+  urls: ['https://www.blaseball.com/events/streamData'],
+  types: ['xmlhttprequest']
+}, ['blocking']);
