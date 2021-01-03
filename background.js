16193cea9415aa2ead9d2d702ea75d25ec905bb2
leo60228 <leo@60228.dev>
2021-01-03 16:22:55 -0500

    sounds

--- background.js
+++ background.js
@@ -1,12 +1,22 @@
+const sounds = {
+  'https://www.blaseball.com/api/chooseIdol': 'sounds/ahhhh.mp3',
+  'https://www.blaseball.com/api/eatADangPeanut': 'sounds/peanut.wav'
+};
+
 function listener(details) {
   if (new URL(details.originUrl).host !== 'www.blaseball.com') return;
 
-  console.log('events');
+  if (sounds.hasOwnProperty(details.url)) {
+    const audio = new Audio(sounds[details.url]);
+    audio.play();
+  } else {
+    console.log(`unknown: ${details.url}`);
+  }
 
   return {};
 }
 
 browser.webRequest.onBeforeRequest.addListener(listener, {
-  urls: ["https://www.blaseball.com/database/eventResults?*"],
-  types: ["xmlhttprequest"]
+  urls: Object.keys(sounds),
+  types: ['xmlhttprequest']
 });
