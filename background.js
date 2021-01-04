e1f66d83ff5798622965695f438521cd37b05659
leo60228 <leo@60228.dev>
2021-01-04 06:28:17 -0500

    Disable unfinished functionality

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 const simpleSounds = {
   'https://www.blaseball.com/api/chooseIdol': 'sounds/ahhhh.mp3',
-  'https://www.blaseball.com/api/eatADangPeanut': 'sounds/peanut.wav'
+  'https://www.blaseball.com/api/eatADangPeanut': 'sounds/peanut.wav',
+  //'https://www.blaseball.com/api/buyUpdateFavoriteTeam': 'sounds/flute.mp3',
 };
 
 function simpleSoundsListener(details) {
@@ -16,32 +17,43 @@ function simpleSoundsListener(details) {
   return {};
 }
 
-function streamDataListener(details) {
+//function streamDataListener(details) {
+//  if (new URL(details.originUrl).host !== 'www.blaseball.com') return;
+//
+//  const filter = browser.webRequest.filterResponseData(details.requestId);
+//  const decoder = new TextDecoder("utf-8");
+//
+//  let buf = '';
+//
+//  filter.ondata = event => {
+//    filter.write(event.data);
+//
+//    const str = decoder.decode(event.data, {stream: true});
+//    buf += str;
+//    const [line, rest] = buf.split('\n');
+//
+//    if (typeof rest === 'undefined') return;
+//
+//    buf = rest;
+//
+//    if (!line.startsWith('data: ')) return;
+//
+//    const json = line.replace(/^data: /, '');
+//
+//    const data = JSON.parse(json);
+//    console.log(data);
+//  };
+//
+//  return {};
+//}
+
+function foghornSoundListener(details) {
   if (new URL(details.originUrl).host !== 'www.blaseball.com') return;
 
-  const filter = browser.webRequest.filterResponseData(details.requestId);
-  const decoder = new TextDecoder("utf-8");
-
-  let buf = '';
-
-  filter.ondata = event => {
-    filter.write(event.data);
-
-    const str = decoder.decode(event.data, {stream: true});
-    buf += str;
-    const [line, rest] = buf.split('\n');
-
-    if (typeof rest === 'undefined') return;
-
-    buf = rest;
-
-    if (!line.startsWith('data: ')) return;
-
-    const json = line.replace(/^data: /, '');
-
-    const data = JSON.parse(json);
-    console.log(data);
-  };
+  if (Math.random() < 0.01) {
+    const audio = new Audio('sounds/foghorn.mp3');
+    audio.play();
+  }
 
   return {};
 }
@@ -51,7 +63,43 @@ browser.webRequest.onBeforeRequest.addListener(simpleSoundsListener, {
   types: ['xmlhttprequest']
 });
 
-browser.webRequest.onBeforeRequest.addListener(streamDataListener, {
-  urls: ['https://www.blaseball.com/events/streamData'],
+//browser.webRequest.onBeforeRequest.addListener(streamDataListener, {
+//  urls: ['https://www.blaseball.com/events/streamData'],
+//  types: ['xmlhttprequest']
+//}, ['blocking']);
+
+browser.webRequest.onBeforeRequest.addListener(foghornSoundListener, {
+  urls: [
+    'https://www.blaseball.com/api/bet',
+    'https://www.blaseball.com/api/bet?*',
+    'https://www.blaseball.com/api/buyADangPeanut',
+    'https://www.blaseball.com/api/buyADangPeanut?*',
+    'https://www.blaseball.com/api/buyADangSquirrel',
+    'https://www.blaseball.com/api/buyADangSquirrel?*',
+    'https://www.blaseball.com/api/buySnack',
+    'https://www.blaseball.com/api/buySnack?*',
+    'https://www.blaseball.com/api/buyUnlockElection',
+    'https://www.blaseball.com/api/buyUnlockElection?*',
+    'https://www.blaseball.com/api/buyUnlockShop',
+    'https://www.blaseball.com/api/buyUnlockShop?*',
+    'https://www.blaseball.com/api/buyUpdateFavoriteTeam',
+    'https://www.blaseball.com/api/buyUpdateFavoriteTeam?*',
+    'https://www.blaseball.com/api/buyVote',
+    'https://www.blaseball.com/api/buyVote?*',
+    'https://www.blaseball.com/api/chooseIdol',
+    'https://www.blaseball.com/api/chooseIdol?*',
+    'https://www.blaseball.com/api/eatADangPeanut',
+    'https://www.blaseball.com/api/eatADangPeanut?*',
+    'https://www.blaseball.com/api/logBeg',
+    'https://www.blaseball.com/api/logBeg?*',
+    'https://www.blaseball.com/api/payTribute',
+    'https://www.blaseball.com/api/payTribute?*',
+    'https://www.blaseball.com/api/updateFavoriteTeam',
+    'https://www.blaseball.com/api/updateFavoriteTeam?*',
+    'https://www.blaseball.com/api/updateSettings',
+    'https://www.blaseball.com/api/updateSettings?*',
+    'https://www.blaseball.com/api/vote',
+    'https://www.blaseball.com/api/vote?*',
+  ],
   types: ['xmlhttprequest']
-}, ['blocking']);
+});
